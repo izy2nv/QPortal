db8944aa3a526fdd7221b374dfc910ab0ff0c90e
Izy2nv <izy2nv@Izy2nvs-MacBook-Pro.local>
2018-03-21 23:27:06 -0400

    Removed unwanted signup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 export const appRoutes: Routes = [
     { path: '', component: LoginComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'signup', component: SignupComponent }
+    { path: 'home', component: HomeComponent }
 ];
